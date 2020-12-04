2ce2bb5473742325399effab80701072d3682dae
Hugo Lobo <hflobo@gmail.com>
2020-12-04 20:55:02 -0300

    versão inicial das avalições está completa. É preciso incrementar com a autorização ainda.

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,3 +1,12 @@
 class Offer < ApplicationRecord
   belongs_to :user
+  has_many :reviews, dependent: :destroy
+
+  def rating
+    reviews = self.reviews
+    total = reviews.inject(0) { |sum, review| sum + review[:rating] }
+    avg = total.fdiv(reviews.count).round(2)
+    avg.nan? ? 'none' : avg
+  end
+
 end
