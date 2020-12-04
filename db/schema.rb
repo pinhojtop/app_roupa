41d7684c8a4000c803794feaf8214c686c77d6a5
Hugo Lobo <hflobo@gmail.com>
2020-12-04 09:07:53 -0300

    Seeds está pronto

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_03_171759) do
+ActiveRecord::Schema.define(version: 2020_12_03_200135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2020_12_03_171759) do
   create_table "offers", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.string "type"
+    t.string "offer_type"
     t.string "size"
     t.string "gender"
     t.string "style"
