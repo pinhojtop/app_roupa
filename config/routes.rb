f5ce392dddd91209ec99b547a45f32527c68fec8
Joao Velloso <joao.velloso@rfb.gov.br>
2020-12-07 17:08:01 -0300

    ajustes no Show-Offers

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :offers do # only: %i[index show new create edit delete]
-    resources :reviews, only: [ :new, :create ]
+    resources :reviews, only: %i[new create]
     resources :rents, only: [:create]
-    
   end
 
-  resources :reviews, only: [:destroy ]
+  resources :reviews, only: [:destroy]
   resources :rents, only: [:destroy]
-
 end
